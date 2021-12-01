f142b56a5096aa66515f16ea3b4087b2b535ebe2
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-12-02 02:20:50 +0700

    chore setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,3 @@
 module.exports = {
-  root: true,
-  extends: ["@nurdiansyah/devel"],
-  ignorePatterns: ["coverage", "index.*", "node_modules", "build", "dist", "**/*.svelte", "script"],
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2020
-  },
-  env: {
-    browser: true,
-    es2020: true,
-    node: true
-  }
+  extends: ["@nurdiansyah/devel"]
 };
